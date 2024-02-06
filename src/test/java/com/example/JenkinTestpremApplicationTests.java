b981afead627c8e9501d16a5abb9d95d254707af
Prem Prakash <gmbuddycp@gmail.com>
2024-02-06 12:55:49 +0530

    first commit

---
+++ src/test/java/com/example/JenkinTestpremApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinTestpremApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertEquals(true, true);
+	}
+
+}
