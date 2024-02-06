c48ffa32786b64215be2bdb01ac86cf7f45f1687
Prem Prakash <gmbuddycp@gmail.com>
2024-02-06 13:55:33 +0530

    second commit

--- src/test/java/com/example/JenkinTestpremApplicationTests.java
+++ src/test/java/com/example/JenkinTestpremApplicationTests.java
@@ -3,13 +3,19 @@ package com.example;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JenkinTestpremApplicationTests {
 
+	
+	public static Logger logger = LoggerFactory.getLogger(JenkinTestpremApplication.class);
+
 	@Test
 	void contextLoads() {
+		logger.info("test case executing .....");
 		assertEquals(true, true);
 	}
 
